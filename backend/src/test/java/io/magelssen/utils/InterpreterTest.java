7bcf7fdcd8f42231090c8f6647c1874f6426f237
Lars Magelssen <lars.magelssen@gmail.com>
2023-04-05 22:01:46 +0200

    test wip

--- backend/src/test/java/io/magelssen/utils/InterpreterTest.java
+++ backend/src/test/java/io/magelssen/utils/InterpreterTest.java
@@ -5,12 +5,12 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class InterpreterTest {
-    @Test
-    void shouldLexNumber() throws Exception {
-        String text = "\"Hello\"         123\" 123he3\" ";
-        var testfile = System.getenv("HOME") + "/projects/java/website/backend/src/test/java/io/magelssen/utils/testfile.md";
-        var lexer = new Interpreter(testfile);
-        lexer.lex();
-    }
+//    @Test
+//    void shouldLexNumber() throws Exception {
+//        String text = "\"Hello\"         123\" 123he3\" ";
+//        var testfile = System.getenv("HOME") + "/projects/java/website/backend/src/test/java/io/magelssen/utils/testfile.md";
+//        var lexer = new Interpreter(testfile);
+//        lexer.lex();
+//    }
 
 }
