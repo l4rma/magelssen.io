f7ac45a8fd5d53e7e5e7cb74433c3c024f10a222
Larma <lars.magelssen@gmail.com>
2023-12-30 20:08:10 +0100

    added contact links

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -1,13 +1,13 @@
 export default function About() {
-    return(
-        <>
-            <h1>About</h1>
-            <div id="about-text">
-                <p>
-                    My name is Lars. I'm an IT consulent mostly working 
-                    with backend development. This is my website.
-                </p>
-            </div>
-        </>
-    )
+	return(
+		<>
+			<h1>About</h1>
+			<hr />
+			<br />
+			<div id="about-text">
+				My name is Lars. I'm an IT consulent mostly working 
+				with backend development. This is my website.
+			</div>
+		</>
+	)
 }
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,7 +1,14 @@
 export default function Home() {
-    return(
-        <h1>
-            &lt;h1&gt;Hello, world!&lt;/h1&gt;
-        </h1>
-    ) 
+	return(
+		<div>
+			<h1>
+				&lt;h1&gt;Hello, world!&lt;/h1&gt;
+			</h1>
+
+			<br />
+			<div class="contact">
+				<a href="https://github.com/l4rma">Github</a> - <a href="https://www.linkedin.com/in/lars-magelssen-5a2a75231">LinkedIn</a> - <a href="mailto:contact@magelssen.io">E-Mail</a>
+			</div>
+		</div>
+	) 
 }
